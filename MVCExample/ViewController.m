b1959c3a104fb570d741de2e7dee7b6d7386aee1
zac1st1k <zacfirst@gmail.com>
2015-01-21 18:26:54 +1100

    Initial Commit

---
+++ MVCExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MVCExample
+//
+//  Created by Zac on 21/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
